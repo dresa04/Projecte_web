ed889a8e3f1b2495ff5bb3ca33e16b44e5a44c98
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-04-11 12:33:28 +0200

    fix(admin):  creacio del superusuari  i  UserLol i MachChampion  a admin .py

--- lol_app/admin.py
+++ lol_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Review, Champion, Match
+from .models import Review, Champion, Match, UserLOL, MatchChampion
 
 admin.site.register(Review)
 admin.site.register(Champion)
 admin.site.register(Match)
+admin.site.register(UserLOL)
+admin.site.register(MatchChampion)
+
