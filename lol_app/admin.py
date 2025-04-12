8505a2cbdede0a0e03d15261c6f8b5c798d00b53
Miquel <miqueel1234@gmail.com>
2025-04-12 17:21:24 +0200

    Canviat admin.py per facilitar la administració de la relació n-aria entre Match, UserLOL i Champion

--- lol_app/admin.py
+++ lol_app/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Review, Champion, Match, UserLOL, MatchChampion
 
+class MatchChampionInline(admin.TabularInline):
+    model = MatchChampion
+    extra = 1
+
+class MatchAdmin(admin.ModelAdmin):
+    inlines = [MatchChampionInline]
+
 admin.site.register(Review)
 admin.site.register(Champion)
-admin.site.register(Match)
+admin.site.register(Match, MatchAdmin)
 admin.site.register(UserLOL)
-admin.site.register(MatchChampion)
-
